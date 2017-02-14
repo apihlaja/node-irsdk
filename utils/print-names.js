0e0b3ab9a01521e1399b4482e0a2c14167983a14
Antti Pihlaja <antti.pihlaja@live.fi>
2017-02-14 02:22:29 +0200

    Add sessionifo TeamName fix

--- utils/print-names.js
+++ utils/print-names.js
@@ -2,30 +2,9 @@
 
 var irsdk = require('../')
 
-var yaml = require('js-yaml')
-
-/** Parser function which should fix TeamName issue
- *
- * @param {string} sessionInfoStr raw session info string
- * @returns {Object} parsed session info or falsy
- */
-var parseSessionInfo = function (sessionInfoStr) {
-  var fixedYamlStr = sessionInfoStr.replace(/TeamName: ([^\n]+)/g, function (match, p1) {
-    if ((p1[0] === '"' && p1[p1.length - 1] === '"') ||
-         (p1[0] === "'" && p1[p1.length - 1] === "'")) {
-      return match // skip if quoted already
-    } else {
-      // 2nd replace is unnecessary atm but its here just in case
-      return "TeamName: '" + p1.replace(/'/g, "''") + "'"
-    }
-  })
-  return yaml.safeLoad(fixedYamlStr)
-}
-
 irsdk.init({
   telemetryUpdateInterval: 1000,
-  sessionInfoUpdateInterval: 1000,
-  sessionInfoParser: parseSessionInfo // replace default parser
+  sessionInfoUpdateInterval: 1000
 })
 
 var iracing = irsdk.getInstance()
